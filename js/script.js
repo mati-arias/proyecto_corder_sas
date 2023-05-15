779e6a0084c0d0d34af121f9206377cee92f6484
Matias Arias <matiasagustinarias@hotmail.com>
2023-05-14 22:01:57 -0300

    add sass

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function search() {
+    var input = document.getElementById("search-bar").value;
+    var resultsList = document.getElementById("results-list");
+    
+    // Aquí puedes agregar tu código para manejar la búsqueda
+    
+    resultsList.innerHTML = "<li>Resultado 1</li><li>Resultado 2</li><li>Resultado 3</li>";
+  }
